52aab42c82661f4c5cd3b8e088f20b532c4c3c66
Kanika Sahni <sahnikanika450@gmail.com>
2025-08-28 17:39:00 +0530

    Time: 3 ms (80%), Space: 13.1 MB (89.67%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         string r;
-        for(int i=0;i<s.length();i++){
+        r.reserve(s.size());
+        for(int i=0;i<s.size();i++){
             char ch=s[i];
             if(!r.empty() && r.back()==ch){
                 r.pop_back();
