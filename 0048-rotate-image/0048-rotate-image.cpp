0f8c8f6573e529bb693709b91eedf2ad3f548d51
Kanika Sahni <sahnikanika450@gmail.com>
2025-08-31 13:17:10 +0530

    Time: 0 ms (100%), Space: 10 MB (92.31%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int r=matrix.size();
+        int c=matrix[0].size();
+        for(int i=0;i<r;i++){
+            for(int j=i+1;j<c;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int i=0;i<r;i++){
+            int s=0,e=c-1;
+            while(s<e){
+                swap(matrix[i][s++],matrix[i][e--]);
+            }
+        }
+    }
+};
