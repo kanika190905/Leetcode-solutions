5b0aa2c22e00ee7c601eaf269c5cc5b33ec1b0a2
Kanika Sahni <sahnikanika450@gmail.com>
2025-09-06 19:28:06 +0530

    Time: 163 ms (79.82%), Space: 12.7 MB (41.04%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -6,13 +6,15 @@ public:
         prime[0]=false;
         prime[1]=false;
         int count=0;
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(prime[i]){
-                count++;
+                for(int j=i*i;j<n;j=j+i){
+                    prime[j]=false;
+                }
             }
-            for(int j=2*i;j<n;j=j+i){
-                prime[j]=false;
-            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) count++;
         }
         return count;
     }
