c1fc5a80536a8ecfcce90c7fbec28020cb45c5e8
Kanika Sahni <sahnikanika450@gmail.com>
2026-01-10 20:13:04 +0530

    Time: 0 ms (100%), Space: 22.2 MB (17.64%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]>0){
+             s.push(asteroids[i]);
+             }
+             else{
+                while(!s.empty() && s.top()>0 && s.top()<abs(asteroids[i]))
+                    s.pop();
+
+                if(!s.empty() && s.top()==abs(asteroids[i])){
+                    s.pop();
+                } 
+                else if(s.empty() || s.top()<0){
+                    s.push(asteroids[i]);
+                }
+             }
+        }  
+        vector<int> ans;
+        while(!s.empty()){
+            ans.push_back(s.top());
+            s.pop();
+        } 
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
