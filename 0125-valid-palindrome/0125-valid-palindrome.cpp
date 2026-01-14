f62077730d632355e8873df8accd17c9800d6bd4
Kanika Sahni <sahnikanika450@gmail.com>
2026-01-14 19:22:18 +0530

    Time: 4 ms (17.35%), Space: 9.8 MB (77.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,32 +1,20 @@
 class Solution {
-    bool valid(char c) {
-        return ( (c >= 'a' && c <= 'z') || 
-                 (c >= 'A' && c <= 'Z') || 
-                 (c >= '0' && c <= '9') );
-    }
-    bool checkPalindrome(string a) {
-        int s = 0;
-        int e = a.length() - 1;
-
-        while (s <= e) {
-            if (tolower(a[s]) != tolower(a[e])) {
-                return false;
-            } else {
-                s++;
-                e--;
+public:
+    bool isPalindrome(string s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(!isalnum(s[l])){
+                l++;
+            }
+            else if(!isalnum(s[r])){
+                r--;
+            }
+            else{
+                if(tolower(s[l])!=tolower(s[r])) return false;
+                l++;
+                r--;
             }
         }
         return true;
-    }
-
-public:
-    bool isPalindrome(string s) {
-        string temp = "";
-        for (int j = 0; j < s.length(); j++) {
-            if (valid(s[j])) {
-                temp.push_back(s[j]);
-            }
-        }
-        return checkPalindrome(temp);
     }
 };
