2894e118ff1100d8ec09339f1b8ebdcb88bde866
Kanika Sahni <sahnikanika450@gmail.com>
2026-01-14 19:58:11 +0530

    Time: 2 ms (15.06%), Space: 22.6 MB (77.95%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j=0;
+        int n=nums.size();
+        int x;
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[j]) {j++;}
+            nums[j]=nums[i];
+        }
+        return j+1;
+    }
+};
