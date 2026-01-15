7ae3af9f4ccb48ec54c10badad3ba4e305ae5393
Kanika Sahni <sahnikanika450@gmail.com>
2026-01-15 23:41:51 +0530

    Time: 65 ms (5.4%), Space: 23.8 MB (81.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0; 
-        for (int j = 0; j < nums.size(); j++) {
-            if (nums[j] != 0) {
-                if (i != j)  // avoid self-swap
-                    nums[i] = nums[j], nums[j] = 0;
-                i++;
+        int i=0;
+        int j=nums.size()-1;
+        while(i<=j){
+            if(nums[i]==0){
+                for(int k=i;k<j;k++){
+                    nums[k]=nums[k+1];
+                }
+                nums[j]=0;
+                j--;
             }
+            if(nums[i]!=0) i++;
         }
     }
 };
