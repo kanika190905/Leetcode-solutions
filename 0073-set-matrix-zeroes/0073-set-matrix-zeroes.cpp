b148674f39a9b9879fdca8f7b8f34e445ebe1942
Kanika Sahni <sahnikanika450@gmail.com>
2026-01-19 22:16:16 +0530

    Time: 0 ms (100%), Space: 18.6 MB (51.07%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0=1;
+        int r=matrix.size();
+        int c=matrix[0].size();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
+                }
+            }
+        }
+       for(int i=1;i<r;i++){
+            for(int j=1;j<c;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+       }
+       
+       if(matrix[0][0]==0){
+        for(int i=0;i<c;i++){
+            matrix[0][i]=0;
+        }
+       }
+       if(col0==0){
+        for(int i=0;i<r;i++){
+            matrix[i][0]=0;
+        }
+       }
+    }
+};
