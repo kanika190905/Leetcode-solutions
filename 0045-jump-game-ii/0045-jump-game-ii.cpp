578258860b1beac6f94a7128c2348e25925a3bb9
Kanika Sahni <sahnikanika450@gmail.com>
2026-02-01 01:36:06 +0530

    Time: 0 ms (100%), Space: 20.5 MB (62.07%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps=0,end=0,maxi=0;
+        for(int i=0;i<nums.size()-1;i++){
+            maxi=max(maxi,i+nums[i]);
+           
+            if(i==end){
+                jumps++;
+                end=maxi;
+            }
+
+           
+        }
+        return jumps;
+    }
+};
