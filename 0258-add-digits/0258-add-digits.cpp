b656f36ada711ff84a9ff931b6e152fb1f142518
Kanika Sahni <sahnikanika450@gmail.com>
2026-02-13 23:19:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.19%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     int addDigits(int num) {
+        // Continue the process until num becomes a single digit
+        while (num >= 10) {
+            int sum = 0;
+            
+            // Inner loop to sum all digits of the current 'num'
+            while (num > 0) {
+                sum += num % 10; // Get the last digit
+                num /= 10;      // Remove the last digit
+            }
+            
+            // Update num with the sum of its digits for the next iteration
+            num = sum;
+        }
         
-        return 1+((num-1)%9);
+        return num;
     }
 };
