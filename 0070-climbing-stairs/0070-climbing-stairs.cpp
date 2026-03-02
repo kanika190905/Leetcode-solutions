dc232b203b8b55564615049e1d1fc83245aca023
Kanika Sahni <sahnikanika450@gmail.com>
2026-03-02 17:16:38 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70.5%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       if(n==0) return 1;
+       if(n==1) return 1;
+       int prev=1;
+       int prev2=1;
+       for(int i=2;i<=n;i++){
+        int ci=prev+prev2;
+        prev2=prev;
+        prev=ci;
+       }
+       return prev;
+    }
+};
