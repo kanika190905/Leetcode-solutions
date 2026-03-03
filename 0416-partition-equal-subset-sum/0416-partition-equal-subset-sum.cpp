f6fb224893636667fb4ddfbca5fd28ecc4391791
Kanika Sahni <sahnikanika450@gmail.com>
2026-03-03 21:15:01 +0530

    Time: 83 ms (78.13%), Space: 13.3 MB (98.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -7,22 +7,19 @@ public:
         }
         if(sum%2!=0) return false;
         vector<bool> prev((sum/2) + 1, false);
-        vector<bool> curr((sum/2) + 1, false);
 
         prev[0] = true;
-         curr[0] = true; 
         if(nums[0] <= (sum/2))
             prev[nums[0]] = true;
         for(int ind=1;ind<nums.size();ind++){
-            for(int target=1;target<=sum/2;target++){
+            for(int target=sum/2;target>=1;target--){
                 bool notTake=prev[target];
                 bool take=false;
                 if(nums[ind]<=target){
                 take=prev[target-nums[ind]];
             }
-            curr[target]=take|notTake;
+            prev[target]=take|notTake;
             }
-            prev=curr;
         }
         return prev[sum/2];
     }
