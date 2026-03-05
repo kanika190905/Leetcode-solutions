da57ab1ed32691b5a5298e1eea5b8ff17755fef7
Kanika Sahni <sahnikanika450@gmail.com>
2026-03-05 16:57:42 +0530

    Time: 45 ms (88.06%), Space: 14.9 MB (81.95%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     int longestCommonSubsequence(vector<int>& nums1, vector<int>& nums2) {
-        int i1=nums1.size();
-        int i2=nums2.size();
-        vector<vector<int>> dp(i1+1,vector<int>(i2+1,-1));
-        int ans=0;
-        for(int i=0;i<=i2;i++) dp[0][i]=0;
-        for(int i=0;i<=i1;i++) dp[i][0]=0;
+        int n = nums1.size();
+    int m = nums2.size();
+int ans=0;
+    vector<int> prev(m+1,0);
+    vector<int> curr(m+1,0);
 
-        for(int i=1;i<=i1;i++){
-             for(int j=1;j<=i2;j++){
-                if(nums1[i-1]==nums2[j-1]){
-             dp[i][j]=1+dp[i-1][j-1];
-             ans=max(ans,dp[i][j]);
+    for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=m;j++){
+
+            if(nums1[i-1] == nums2[j-1]){
+                curr[j] = 1 + prev[j-1];
+                ans=max(ans,curr[j]);
+            }
+            else{
+                curr[j] = 0;
+            }
+
         }
-        else  dp[i][j]=0;
-             }
-        }
-        
-        return ans;
+
+        prev = curr;
+    }
+
+    return ans;
     }
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int ans = longestCommonSubsequence(nums1,nums2);
