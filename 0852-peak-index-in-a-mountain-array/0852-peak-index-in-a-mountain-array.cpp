c5525ca65d4b91d2de89a2f59a166be188182bd5
Kanika Sahni <sahnikanika450@gmail.com>
2026-03-14 22:15:59 +0530

    Time: 0 ms (100%), Space: 63.5 MB (77.9%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int high=arr.size()-1;
-        int low=0;int ans=-1;
+        int low=0;
         while(low<=high){
             int mid=(low+high)/2;
             if(arr[mid]<arr[mid+1]){
                 low=mid+1;
             }
             else {
-                ans=mid;
                 high=mid-1;
             }
         }
-        return ans;
+        return low;
     }
 };
