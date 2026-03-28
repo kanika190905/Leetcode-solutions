da04396433b63f27e43b7c19ab263ae0050a24f2
Kanika Sahni <sahnikanika450@gmail.com>
2026-03-29 00:36:07 +0530

    Time: 53 ms (49.59%), Space: 15.8 MB (61.07%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -20,7 +20,22 @@ public:
     }
     bool isMatch(string s, string p) {
         int m=s.size(),n=p.size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,s,p,dp);
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,0));
+        dp[0][0]=true;
+        for(int j=1;j<=n;j++){
+            if(p[j-1]=='*') dp[0][j]=dp[0][j-1];
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                      dp[i][j]=dp[i][j-1]||dp[i-1][j]; //empty,one char
+                    }
+              else  dp[i][j]=false;   
+            }
+        }
+        return dp[m][n];
     }
 };
