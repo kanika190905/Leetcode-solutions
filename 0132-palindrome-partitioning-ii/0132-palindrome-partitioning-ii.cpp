8cb78e899b747841c6be1310db71dba7dd96ef07
Kanika Sahni <sahnikanika450@gmail.com>
2026-04-05 19:13:34 +0530

    Time: 1804 ms (5.67%), Space: 9.4 MB (77.52%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    bool isPalindrome(string& s){
-        int i=0,j=s.size()-1;
-        while(i<=j){
-            if(s[i]!=s[j]) return false;
-            i++;j--;
+    bool isPalindrome(string &s, int i, int j){
+        while(i <= j){
+            if(s[i] != s[j]) return false;
+            i++; j--;
         }
         return true;
     }
-    int f(int i,string& s,vector<int>& dp){
-        if(i==s.size()) return 0;
-        string temp="";
-        if(dp[i]!=-1) return dp[i];
-        int mini=INT_MAX;
-        for(int j=i;j<s.size();j++){
-            temp+=s[j];
-            int cost=INT_MAX;
-            if(isPalindrome(temp)){
-                cost=1+f(j+1,s,dp);
+
+    int f(int i, string& s, vector<int>& dp){
+        if(i == s.size()) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int mini = INT_MAX;
+
+        for(int j = i; j < s.size(); j++){
+            if(isPalindrome(s, i, j)){
+                int cost = 1 + f(j+1, s, dp);
+                mini = min(mini, cost);
             }
-            mini=min(mini,cost);
-           
         }
-        return dp[i]=mini;
+
+        return dp[i] = mini;
     }
+
     int minCut(string s) {
-        vector<int> dp(s.size(),-1);
-        return f(0,s,dp)-1;
+        vector<int> dp(s.size(), -1); // FIXED
+        return f(0, s, dp) - 1;
     }
 };
