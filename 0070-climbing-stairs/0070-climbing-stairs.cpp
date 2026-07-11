1a7587d7d1c3822adc7548051b8f2f341af210d9
Kanika Sahni <sahnikanika450@gmail.com>
2026-07-11 10:48:33 +0530

    Time: 3 ms (0.36%), Space: 9.3 MB (19.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
+    int noOfWays(int i,int n,vector<vector<int>>& dp){
+        if(n==0){
+            return 1;
+        }
+        if(n<0) return 0;
+        if(dp[i][n]!=-1) return dp[i][n];
+        int take1=noOfWays(i+1,n-1,dp);
+        int take2=noOfWays(i+2,n-2,dp);
+        return dp[i][n]=take1+take2;
+    }
     int climbStairs(int n) {
-       if(n==0) return 1;
-       if(n==1) return 1;
-       int prev=1;
-       int prev2=1;
-       for(int i=2;i<=n;i++){
-        int ci=prev+prev2;
-        prev2=prev;
-        prev=ci;
-       }
-       return prev;
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return noOfWays(0,n,dp);
     }
 };
