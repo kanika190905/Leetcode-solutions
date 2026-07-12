51c5358d9ebdee7681ebf8c05117da11039a7e20
Kanika Sahni <sahnikanika450@gmail.com>
2026-07-12 22:30:14 +0530

    Time: 51 ms (27.49%), Space: 75.7 MB (33.49%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-    ListNode* findMiddle(ListNode* head){
+ListNode* findMiddle(ListNode* head){
         ListNode* slow=head;
         ListNode* fast=head->next;
         while(fast && fast->next){
@@ -20,19 +20,27 @@ public:
         return slow;
     }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL || list2==NULL){
-            return list1==NULL?list2:list1;
-        }
 
-        if(list1->val<=list2->val){
-            list1->next=mergeTwoLists(list1->next,list2);
-            return list1;
+    ListNode* dummy = new ListNode(-1);
+    ListNode* temp = dummy;
+
+    while(list1 && list2){
+        if(list1->val <= list2->val){
+            temp->next = list1;
+            list1 = list1->next;
         }
         else{
-             list2->next=mergeTwoLists(list1,list2->next);
-            return list2;
+            temp->next = list2;
+            list2 = list2->next;
         }
+        temp = temp->next;
     }
+
+    if(list1) temp->next = list1;
+    else temp->next = list2;
+
+    return dummy->next;
+}
     ListNode* sortList(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
 
