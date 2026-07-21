b2812f127173dd4b1f318e51870bf01499462d47
Kanika Sahni <sahnikanika450@gmail.com>
2026-07-21 17:03:37 +0530

    Time: 8 ms (7.81%), Space: 19.5 MB (61.3%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,24 +1,63 @@
 class Solution {
 public:
-    void dfs(int start,vector<vector<int>>& isConnected,vector<bool>& vis){
-        vis[start]=1;
-       for(int j=0;j<isConnected.size();j++){
-            if(isConnected[start][j] == 1 && !vis[j]) {
-            dfs(j, isConnected, vis);}
-       }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-       vector<bool> vis(n,0);
-       
-        int ans=0;
-        
-        for(int i=0;i<isConnected.size();i++){
-            if(!vis[i]){
-                dfs(i,isConnected,vis);
-                ans++;
+    class DSU {
+    public:
+        vector<int> parent, rank;
+
+        DSU(int n) {
+            parent.resize(n);
+            rank.resize(n, 0);
+
+            for (int i = 0; i < n; i++)
+                parent[i] = i;
+        }
+
+        int findParent(int node) {
+            if (parent[node] == node)
+                return node;
+
+            return parent[node] = findParent(parent[node]);
+        }
+
+        void unionByRank(int u, int v) {
+            int pu = findParent(u);
+            int pv = findParent(v);
+
+            if (pu == pv)
+                return;
+
+            if (rank[pu] < rank[pv]) {
+                parent[pu] = pv;
+            }
+            else if (rank[pu] > rank[pv]) {
+                parent[pv] = pu;
+            }
+            else {
+                parent[pv] = pu;
+                rank[pu]++;
             }
         }
-return ans;
+    };
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        DSU ds(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    ds.unionByRank(i, j);
+                }
+            }
+        }
+
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (ds.findParent(i) == i)
+                provinces++;
+        }
+
+        return provinces;
     }
 };
