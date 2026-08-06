0d795b55edce80ba26c0ffb3e986114c75725715
Kanika Sahni <sahnikanika450@gmail.com>
2026-08-06 22:52:15 +0530

    Time: 196 ms (29.15%), Space: 165.3 MB (31.26%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int last=-1,start=-1;
+    bool dfs(int node,vector<char> &color,vector<vector<int>> &adj,vector<int>& parent){
+        color[node]='G';
+        for(auto it:adj[node]){
+            if(color[it]=='W'){
+                 parent[it]=node;
+                if(dfs(it,color,adj,parent)) {
+                    color[node] = 'B';
+                    if(start!=-1) return true;
+                    return true;}
+            }
+            else{
+                if(color[it]=='G') {
+                    color[node] = 'B';
+                    start=node;
+                    last=it;
+                    return true;
+                }
+            }
+        }
+        color[node]='B';
+        return false;
+    }
+    int longestCycle(vector<int>& edges) {
+        vector<vector<int>> adj(edges.size());
+        for(int i=0;i<edges.size();i++){
+            if(edges[i]!=-1)
+            adj[i].push_back(edges[i]);
+        }
+        vector<char> color(edges.size(),'W');
+        vector<int> parent(edges.size(),-1);
+        int maxi=INT_MIN;
+        vector<int> ans;
+        bool flag=0;
+        for(int i=0;i<edges.size();i++){
+            if(color[i]=='W'){
+                ans.clear();
+                if(dfs(i,color,adj,parent)){
+    
+                    int x=last;
+                    ans.push_back(start);
+                    while(start!=x){
+                        start=parent[start];
+                        ans.push_back(start);
+                    }
+                    flag=1;
+                    int y=ans.size();
+                    maxi=max(maxi,y);
+                }
+            }
+        }
+        if(!flag){
+            return -1;
+        }
+        else{
+            return maxi;
+        }
+    }
+};
