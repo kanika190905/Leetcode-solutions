cd607288307b504ffa77155d070527bb8ced0d3d
Kanika Sahni <sahnikanika450@gmail.com>
2026-08-11 00:01:12 +0530

    Time: 62 ms (16.77%), Space: 30.8 MB (12.02%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    int helper(int i, int m, vector<int>& piles, vector<vector<int>>& dp, vector<int>& suffix) {
+    int helper(int i, bool alice,int m, vector<int>& piles, vector<vector<vector<int>>>& dp) {
         if (i >= piles.size()) return 0;
 
-        if (dp[i][m] != -1) return dp[i][m];
+        int stones=0;
+        int result=(alice==1)? INT_MIN:INT_MAX;
+        if (dp[i][m][alice] != -1)
+            return dp[i][m][alice];
 
-        int maxi = 0;
-
-        for (int j = 1; j <= 2 * m && i + j <= piles.size(); j++) {
-            int opponent = helper(i + j, max(m, j), piles, dp, suffix);
-            int current = suffix[i] - opponent;
-
-            maxi = max(maxi, current);
+        for(int x=1;x<=min(2*m,(int)piles.size()-i);x++){
+            stones+=piles[i+x-1];
+            if(alice){
+                result=max(result,stones+helper(i+x,0,max(m,x),piles,dp));
+            }
+            else{
+                result=min(result,helper(i+x,1,max(m,x),piles,dp));
+            }
         }
+        
 
-        return dp[i][m] = maxi;
+        return  dp[i][m][alice]=result;
     }
 
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
 
-        vector<int> suffix(n + 1, 0);
+        vector<vector<vector<int>>> dp(
+    n,
+    vector<vector<int>>(n + 1,
+        vector<int>(2, -1)
+    )
+);
 
-        for (int i = n - 1; i >= 0; i--) {
-            suffix[i] = piles[i] + suffix[i + 1];
-        }
-
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-
-        return helper(0, 1, piles, dp, suffix);
+        return helper(0,1, 1, piles, dp);
     }
 };
