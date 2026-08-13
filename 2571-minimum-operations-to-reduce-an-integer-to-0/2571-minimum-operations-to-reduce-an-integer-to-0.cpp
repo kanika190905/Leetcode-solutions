375bf5847c7f6d028d93cad447652677a3812fff
Kanika Sahni <sahnikanika450@gmail.com>
2026-08-14 03:09:38 +0530

    Time: 3 ms (6.94%), Space: 13.2 MB (5.18%) - LeetHub

--- 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int solve(int n){
+    int solve(int n,vector<int>& dp){
         if(n==0){
             return 0;
         }
         if((n&(n-1))==0) return 1;
+        if(dp[n]!=-1) return dp[n];
         int low=1;
         while(low*2<n){
             low*=2;
         }
         int high=low*2;
-        return 1+min(solve(n-low),solve(high-n));
+        return dp[n]=1+min(solve(n-low,dp),solve(high-n,dp));
     }
     int minOperations(int n) {
-        return solve(n);
+        vector<int> dp(n + 1, -1);
+        return solve(n,dp);
     }
 };
