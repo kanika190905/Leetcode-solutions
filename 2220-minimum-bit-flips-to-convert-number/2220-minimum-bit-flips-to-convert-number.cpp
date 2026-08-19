2a21c752cd22e6272f742fd8a5b44ea4e797713f
Kanika Sahni <sahnikanika450@gmail.com>
2026-08-19 16:35:51 +0530

    Time: 0 ms (100%), Space: 7.9 MB (60.94%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+       int n=start|goal;
+       int x=start&goal;
+      int ans= n-x;
+      int cnt=0;
+      for(int i=0;i<32;i++){
+        if(ans&(1<<i)){
+            cnt++;
+        }
+      }
+      return cnt;
+    }
+};
