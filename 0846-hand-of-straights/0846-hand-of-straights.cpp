20364aebf32dc3b477e4a969a3d3a65bbb820c95
Kanika Sahni <sahnikanika450@gmail.com>
2026-08-21 19:38:01 +0530

    Time: 48 ms (15.53%), Space: 32.4 MB (42.82%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -2,40 +2,17 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if(hand.size()%groupSize!=0) return false;
-        if(groupSize==1) return true;
-        unordered_map<int,int> freq;
-        priority_queue<int, vector<int>, greater<int>> pq;
+        map<int,int> freq;
         for(int x:hand){
             freq[x]++;
-            if(freq[x]==1){
-                pq.push(x);
-            }
         }
         int k=groupSize;
-        int n=hand.size()/groupSize;
-        int prev=-1;
-        vector<int> temp;
-        while(!pq.empty() || !temp.empty()){
-            if(k==0){
-                k=groupSize;
-                for(int y:temp){
-                    pq.push(y);
-                }
-                temp.clear();
-                prev=-1;
+        for(auto [x,cnt]:freq){
+            if(freq[x]==0) continue;
+            for(int i=0;i<k;i++){
+                if(freq[x+i]==0) return false;
+                freq[x+i]-=cnt;
             }
-           int curr=pq.top();
-           pq.pop();
-           if(prev!=-1 && curr-prev!=1) return false;
-           k--;
-           freq[curr]--;
-           prev=curr;
-           if(freq[curr]){
-            temp.push_back(curr);
-           }
-        }
-        for(auto x:freq){
-            if(freq[x.first]!=0) return false;
         }
         return true;
     }
