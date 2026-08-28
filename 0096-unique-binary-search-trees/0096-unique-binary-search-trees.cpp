3aeb89d72fd94e88ef6e2c698d5bfc62c16e893c
Kanika Sahni <sahnikanika450@gmail.com>
2026-08-28 23:00:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (20.8%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int solve(int n,vector<int> &dp){
-        if(n<=1) return 1;
-        int ans=0;
-        if(dp[n]!=-1) return dp[n];
-        for(int root=1;root<=n;root++){
-            int left=solve(root-1,dp);
-            int right=solve(n-root,dp);
-            ans+=left*right;
-        }
-        return dp[n]=ans;
-    }
+
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+        int ans=0;
+        for(int root=1;root<=i;root++){
+            int left=dp[root-1];
+            int right=dp[i-root];
+            ans+=left*right;
+            
+        }
+        dp[i]=ans;
+        }
+        return dp[n];
     }
 };
